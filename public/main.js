dd5eade87c650b6afe74b491f5e905ffd6a50f6a
amanshawar <amanshawar_a@hotmail.com>
2018-11-21 14:46:10 +0200

    reorder the code #18

--- public/main.js
+++ public/main.js
@@ -3,30 +3,28 @@ function event1() {
 
 fetch('/search?query='+text)
 .then(response => {
-return response.json();
+  return response.json();
 })
 .then(function (data){
-console.log(data);
-var arrayofres=[];
-var counter=0;
-for (var i = 0; i < data.length; i++) {
-  if(data[i].startsWith(text))
-  {
-    arrayofres.push(data[i]);
-    counter++;
-    if(counter==5)
+  var arrayofres=[];
+  var counter=0;
+  for (var i = 0; i < data.length; i++) {
+    if(data[i].startsWith(text))
     {
-      break;
+      arrayofres.push(data[i]);
+      counter++;
+      if(counter==5)
+      {
+        break;
+      }
     }
-  }
 }
 console.log(arrayofres);
-
-
     var ul = document.createElement("ul");
     ul.setAttribute('id', 'ulid');
     for (var i =0; i < arrayofres.length; i++){
     var li = document.createElement("li");
+    li.setAttribute('class', 'licss');
     var valueofarray=document.createTextNode(arrayofres[i]);
     li.appendChild(valueofarray);
     ul.appendChild(li);
