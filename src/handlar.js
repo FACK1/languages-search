d94476737b956d9865706b6b16722891552b52a9
amanshawar <amanshawar_a@hotmail.com>
2018-11-21 10:37:08 +0200

    make search #14

--- src/handlar.js
+++ src/handlar.js
@@ -15,7 +15,8 @@ function handler(request, response){
                  response.writeHead(200, {'Content-Type': 'text/html'})
                  response.end(html)
              })
-         }else if(endpoint.includes('/public/')){
+      }
+         else if(endpoint.includes('/public/')){
              const filePath =  path.join(__dirname, '..', request.url);
              const extension = request.url.split('.')[1];
              const contentTypeMapping = {
@@ -31,10 +32,25 @@ function handler(request, response){
                 response.writeHead(200, {'content-Type': contentTypeMapping[extension]})
                 response.end(file)
             })
-         }
-         else if(endpoint==='/search'){
+      }
+         else if(endpoint.includes('/search')){
+           console.log(endpoint.split('=')[1]);
 
-         }
+           var allData = '';
+           request.on('data', function(query) {
+             allData += query;
+           })
+           request.on('end', function() {
+             var filePath = path.join(__dirname, 'languages.txt');
+             var data = fs.readFileSync(filePath);
+             var arrayoflang=[]
+             arrayoflang=  data.toString().toLowerCase().split('\n');
+             var JSONResults = JSON.stringify(arrayoflang);
+             
+             response.writeHead(200,{"content-type":"text/javascript"});
+             response.end(JSONResults);
+           })
+      }
          else {
              response.writeHead(404, {'Content-Type': 'text/html'})
              response.end('<h1>Page Not Found</h1>')
